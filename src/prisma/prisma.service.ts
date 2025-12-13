1da5ba0ffb5cb80b8e1976f03d82d6ab6a53bb92
ArzubekDev <arzubekmain@gmail.com>
2025-12-13 19:53:20 +0600

    Fix: PrismaClient

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -1,19 +1,19 @@
-import { Injectable } from '@nestjs/common';
-import { ConfigService } from '@nestjs/config';
-import { PrismaPg } from '@prisma/adapter-pg';
-import { PrismaClient } from '@prisma/client';
+// import { Injectable } from '@nestjs/common';
+// import { ConfigService } from '@nestjs/config';
+// import { PrismaPg } from '@prisma/adapter-pg';
+// import { PrismaClient } from '@prisma/client';
 
-@Injectable()
-export class PrismaService extends PrismaClient {
-constructor(config: ConfigService) {
-  const url = config.get<string>('DATABASE_URL');
-  console.log('DB URL:', url);
+// @Injectable()
+// export class PrismaService extends PrismaClient {
+// constructor(config: ConfigService) {
+//   const url = config.get<string>('DATABASE_URL');
+//   console.log('DB URL:', url);
 
-  const adapter = new PrismaPg({ url });
-  super({ adapter });
-}
+//   const adapter = new PrismaPg({ url });
+//   super({ adapter });
+// }
 
-}
+// }
 
 // import { Injectable } from '@nestjs/common';
 // import { ConfigService } from '@nestjs/config';
@@ -37,3 +37,14 @@ constructor(config: ConfigService) {
 //     });
 //   }
 // }
+
+
+import { Injectable } from '@nestjs/common';
+import { PrismaClient } from '@prisma/client';
+
+@Injectable()
+export class PrismaService extends PrismaClient {
+  constructor() {
+    super();
+  }
+}
