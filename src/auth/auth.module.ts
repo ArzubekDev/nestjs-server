c3a29a1be05aae786786e0b7db2b1eac97445ce4
ArzubekDev <arzubekmain@gmail.com>
2025-12-13 22:32:01 +0600

    upload auth.module.ts, prisma.service.ts

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,11 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
+import { AuthController } from './auth.controller';
 import { PrismaModule } from 'src/prisma/prisma.module';
 import { JwtService } from 'src/config/jwt.service';
 
 @Module({
-  imports: [PrismaModule],   // PrismaService үчүн
-  providers: [AuthService, JwtService], // Бул жерде JwtService provider катары кошулат
+  imports: [PrismaModule],
+  controllers: [AuthController], 
+  providers: [AuthService, JwtService],
   exports: [AuthService],
 })
 export class AuthModule {}
--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -1,41 +1,24 @@
 import { Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
-import { PrismaPg } from '@prisma/adapter-pg';
 import { PrismaClient } from '@prisma/client';
+import { PrismaPg } from '@prisma/adapter-pg';
+import pg from 'pg';
 
 @Injectable()
 export class PrismaService extends PrismaClient {
-constructor(config: ConfigService) {
-  const url = config.get<string>('DATABASE_URL');
-  console.log('DB URL:', url);
+  constructor(config: ConfigService) {
+    const url = config.get<string>('DATABASE_URL');
 
-  const adapter = new PrismaPg({ url });
-  super({ adapter });
+    const pool = new pg.Pool({
+      connectionString: url,
+    });
+
+    const adapter = new PrismaPg(pool);
+
+    super({
+      adapter,
+    });
+  }
 }
-
-}
-
-// import { Injectable } from '@nestjs/common';
-// import { ConfigService } from '@nestjs/config';
-// import { PrismaClient } from '@prisma/client';
-// import { PrismaPg } from '@prisma/adapter-pg';
-// import pg from 'pg';
-
-// @Injectable()
-// export class PrismaService extends PrismaClient {
-//   constructor(config: ConfigService) {
-//     const url = config.get<string>('DATABASE_URL');
-
-//     const pool = new pg.Pool({
-//       connectionString: url,
-//     });
-
-//     const adapter = new PrismaPg(pool);
-
-//     super({
-//       adapter,
-//     });
-//   }
-// }
 
 
