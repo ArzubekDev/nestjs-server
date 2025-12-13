90529ba4a94a639d6c190be1ac0053dd4d7f0038
ArzubekDev <arzubekmain@gmail.com>
2025-12-13 20:08:14 +0600

    Fix: prisma

--- prisma/migrations/20251213140728_auth/migration.sql
+++ prisma/migrations/20251213140728_auth/migration.sql
@@ -1,3 +1,6 @@
+-- CreateEnum
+CREATE TYPE "Role" AS ENUM ('USER', 'ADMIN', 'OWNER');
+
 -- CreateTable
 CREATE TABLE "User" (
     "id" TEXT NOT NULL,
@@ -5,6 +8,7 @@ CREATE TABLE "User" (
     "email" TEXT NOT NULL,
     "password" TEXT NOT NULL,
     "level" TEXT NOT NULL DEFAULT 'Bronze',
+    "role" "Role" NOT NULL DEFAULT 'USER',
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updatedAt" TIMESTAMP(3) NOT NULL,
 
