aa5657c8587ca901f354c1760a5dd74621e9e990
ArzubekDev <arzubekmain@gmail.com>
2026-01-22 23:01:12 +0600

    Fix: main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,37 @@
+// import { NestFactory } from '@nestjs/core';
+// import { AppModule } from './app.module';
+// import { ValidationPipe } from '@nestjs/common';
+// import cookieParser from 'cookie-parser';
+
+// async function bootstrap() {
+//   const app = await NestFactory.create(AppModule);
+
+//   app.use(cookieParser()); 
+
+//   app.setGlobalPrefix('api');
+
+//   app.enableCors({
+//     origin: [
+//       'http://localhost:3000',
+//       'https://devarena-neon.vercel.app',
+//     ],
+//     credentials: true,
+//   });
+
+//   app.useGlobalPipes(
+//     new ValidationPipe({
+//       whitelist: true,
+//       forbidNonWhitelisted: true,
+//       transform: true,
+//     }),
+//   );
+
+//   const port = process.env.PORT || 4000; 
+//   await app.listen(port, '0.0.0.0');
+
+// }
+// bootstrap();
+
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
@@ -6,7 +40,11 @@ import cookieParser from 'cookie-parser';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  app.use(cookieParser()); 
+  // ✅ Express instance
+  const server = app.getHttpAdapter().getInstance();
+  server.set('trust proxy', 1);
+
+  app.use(cookieParser(process.env.COOKIE_SECRET));
 
   app.setGlobalPrefix('api');
 
@@ -26,8 +64,8 @@ async function bootstrap() {
     }),
   );
 
-  const port = process.env.PORT || 4000; 
+  const port = process.env.PORT || 4000;
   await app.listen(port, '0.0.0.0');
-
 }
+
 bootstrap();
